a5b2e60d6b19c0f59612ef1fe2b4c1a68f31720d
yangchuang64 <ytf2737179@163.com>
2019-09-29 10:52:03 +0800

    修改qos agent请求，增加test

--- lib/qos/gov/types/deposit.go
+++ lib/qos/gov/types/deposit.go
@@ -4,7 +4,7 @@ package types
 type Deposit struct {
 	Depositor  string `json:"depositor"`   //  Address of the depositor
 	ProposalID int64  `json:"proposal_id"` //  proposalID of the proposal
-	Amount     BigInt `json:"amount"`      //  Deposit amount
+	Amount     string `json:"amount"`      //  Deposit amount
 }
 
 type Deposits []Deposit
--- lib/qos/gov/types/proposal.go
+++ lib/qos/gov/types/proposal.go
@@ -1,31 +1,28 @@
 package types
 
 import (
-	"math/big"
 	"time"
 )
 
 type Proposal struct {
-	ProposalContent `json:"proposal_content"` // Proposal content interface
-
-	ProposalID int64 `json:"proposal_id"` //  ID of the proposal
-
-	Status           byte        `json:"proposal_status"`    //  Status of the Proposal {Pending, Active, Passed, Rejected}
-	FinalTallyResult TallyResult `json:"final_tally_result"` //  Result of Tallys
-
-	SubmitTime     time.Time `json:"submit_time"`      //  Time of the block where TxGovSubmitProposal was included
-	DepositEndTime time.Time `json:"deposit_end_time"` // Time that the Proposal would expire if deposit amount isn't met
-	TotalDeposit   BigInt    `json:"total_deposit"`    //  Current deposit on this proposal. Initial value is set at InitialDeposit
-
-	VotingStartTime   time.Time `json:"voting_start_time"` //  Time of the block where MinDeposit was reached. -1 if MinDeposit is not reached
-	VotingStartHeight int64     `json:"voting_start_height"`
-	VotingEndTime     time.Time `json:"voting_end_time"` // Time that the VotingPeriod for this proposal will end and votes will be tallied
+	ProposalContent   ProposalContent `json:"proposal_content"`   // Proposal content interface
+	ProposalID        int64           `json:"proposal_id"`        //  ID of the proposal
+	Status            string          `json:"proposal_status"`    //  Status of the Proposal {Pending, Active, Passed, Rejected}
+	FinalTallyResult  TallyResult     `json:"final_tally_result"` //  Result of Tallys
+	SubmitTime        time.Time       `json:"submit_time"`        //  Time of the block where TxGovSubmitProposal was included
+	DepositEndTime    time.Time       `json:"deposit_end_time"`   // Time that the Proposal would expire if deposit amount isn't met
+	TotalDeposit      string          `json:"total_deposit"`      //  Current deposit on this proposal. Initial value is set at InitialDeposit
+	VotingStartTime   time.Time       `json:"voting_start_time"`  //  Time of the block where MinDeposit was reached. -1 if MinDeposit is not reached
+	VotingStartHeight int64           `json:"voting_start_height"`
+	VotingEndTime     time.Time       `json:"voting_end_time"` // Time that the VotingPeriod for this proposal will end and votes will be tallied
 }
 
 type ProposalContent struct {
-	Type  string               `json:"type"`
-	Value ProposalContentValue `json:"value"`
+	Type        string `json:"title"`
+	Description string `json:"description"`
+	Deposit     string `json:"deposit"`
 }
+
 type ProposalContentValue struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
@@ -34,12 +31,8 @@ type ProposalContentValue struct {
 
 // Tally Results
 type TallyResult struct {
-	Yes        BigInt `json:"yes"`
-	Abstain    BigInt `json:"abstain"`
-	No         BigInt `json:"no"`
-	NoWithVeto BigInt `json:"no_with_veto"`
-}
-
-type BigInt struct {
-	i *big.Int
+	Yes        string `json:"yes"`
+	Abstain    string `json:"abstain"`
+	No         string `json:"no"`
+	NoWithVeto string `json:"no_with_veto"`
 }
